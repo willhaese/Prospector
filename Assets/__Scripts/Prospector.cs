aa472d826a9ec02efa2503e535f172c446f35da8
William Haesemeyer <willhaese@gmail.com>
2017-07-12 22:36:47 -0400

    Implemented Scoring

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -2,10 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// An enum to handle all the possible scoring events
+public enum ScoreEvent
+{
+    draw,
+    mine,
+    mineGold,
+    gameWin,
+    gameLoss
+}
+
 
 public class Prospector : MonoBehaviour {
 
 	static public Prospector 	S;
+    static public int SCORE_FROM_PREV_ROUND = 0;
+    static public int HIGH_SCORE = 0;
 	public Deck					deck;
 	public TextAsset			deckXML;
     public Layout layout;
@@ -18,9 +30,23 @@ public class Prospector : MonoBehaviour {
     public List<CardProspector> tableau;
     public List<CardProspector> discardPile;
     public List<CardProspector> drawPile;
+    // Fields to track score info
+    public int chain = 0; // of cards in this run
+    public int scoreRun = 0;
+    public int score = 0;
 
     void Awake(){
 		S = this;
+        // Check for a high score in PlayerPrefs
+        if (PlayerPrefs.HasKey("ProspectorHighScore"))
+        {
+            HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
+        }
+        // Add the score from last round, which will be >0 if it was a win
+        score += SCORE_FROM_PREV_ROUND;
+        // And reset the SCORE_FROM_PREV_ROUND
+        SCORE_FROM_PREV_ROUND = 0;
+
 	}
 
 	void Start() {
@@ -48,6 +74,7 @@ public class Prospector : MonoBehaviour {
                 MoveToDiscard(target); // Moves the target to the discardPile
                 MoveToTarget(Draw());  // Moves the next drawn card to the target
                 UpdateDrawPile();      // Restacks the drawPile
+                ScoreManager(ScoreEvent.draw);
                 break;
             case CardState.tableau:
                 // Clicking a card in the tableau will check if it's a valid play
@@ -66,10 +93,13 @@ public class Prospector : MonoBehaviour {
                                          // Yay! It's a valid card.
                 tableau.Remove(cd); // Remove it from the tableau List
                 MoveToTarget(cd);   // Make it the target card
-
+                SetTableauFaces();  // Update tableau card face-ups
+                ScoreManager(ScoreEvent.mine);
                 break;
         }
-
+    
+        // Check to see whether the game is over or not
+        CheckForGameOver();
     }
     // Moves the current target to the discardPile
     void MoveToDiscard(CardProspector cd)
@@ -136,6 +166,23 @@ public class Prospector : MonoBehaviour {
         drawPile.RemoveAt(0);            // Then remove it from List<> drawPile
         return (cd);                      // And return it
     }
+
+    // Convert from the layoutID int to the CardProspector with that ID
+    CardProspector FindCardByLayoutID(int layoutID)
+    {
+        foreach (CardProspector tCP in tableau)
+        {
+            // Search through all cards in the tableau List<>
+            if (tCP.layoutID == layoutID)
+            {
+                // If the card has the same ID, return it
+                return (tCP);
+            }
+        }
+        // If it's not found, return null
+        return (null);
+    }
+
     // LayoutGame() positions the initial tableau of cards, a.k.a. the "mine"
     void LayoutGame()
     {
@@ -169,6 +216,17 @@ public class Prospector : MonoBehaviour {
             cp.SetSortingLayerName(tSD.layerName); // Set the sorting layers
             tableau.Add(cp); // Add this CardProspector to the List<> tableau
         }
+
+        // Set which cards are hiding others
+        foreach (CardProspector tCP in tableau)
+        {
+            foreach (int hid in tCP.slotDef.hiddenBy)
+            {
+                cp = FindCardByLayoutID(hid);
+                tCP.hiddenBy.Add(cp);
+            }
+        }
+
         // Set up the initial target card
         MoveToTarget(Draw());
         // Set up the Draw pile
@@ -204,6 +262,110 @@ public class Prospector : MonoBehaviour {
         if (c0.rank == 13 && c1.rank == 1) return (true);
         // Otherwise, return false
         return (false);
+    }
+    // This turns cards in the Mine face-up or face-down
+    void SetTableauFaces()
+    {
+        foreach (CardProspector cd in tableau)
+        {
+            bool fup = true; // Assume the card will be face-up
+            foreach (CardProspector cover in cd.hiddenBy)
+            {
+                // If either of the covering cards are in the tableau
+                if (cover.state == CardState.tableau)
+                {
+                    fup = false; // then this card is face-down
+                }
+            }
+            cd.faceUP = fup; // Set the value on the card
+        }
+    }
+    // Test whether the game is over
+    void CheckForGameOver()
+    {
+        // If the tableau is empty, the game is over
+        if (tableau.Count == 0)
+        {
+            // Call GameOver() with a win
+            GameOver(true);
+            return;
+        }
+        // If there are still cards in the draw pile, the game's not over
+        if (drawPile.Count > 0)
+        {
+            return;
+        }
+        // Check for remaining valid plays
+        foreach (CardProspector cd in tableau)
+        {
+            if (AdjacentRank(cd, target))
+            {
+                // If there is a valid play, the game's not over
+                return;
+            }
+        }
+        // Since there are no valid plays, the game is over
+        // Call GameOver with a loss
+        GameOver(false);
+    }
+    // Called when the game is over. Simple for now, but expandable
+    void GameOver(bool won)
+    {
+        if (won)
+        {
+            ScoreManager(ScoreEvent.gameWin);      // This replaces the old line
+        }
+        else
+        {
+            ScoreManager(ScoreEvent.gameLoss);     // This replaces the old line
+        }
+        // Reload the scene, resetting the game
+        Application.LoadLevel("__Prospector_Scene_0");
+    }
+    // ScoreManager handles all of the scoring
+    void ScoreManager(ScoreEvent sEvt)
+    {
+        switch (sEvt)
+        {
+            // Same things need to happen whether it's a draw, a win, or a loss
+            case ScoreEvent.draw: // Drawing a card
+            case ScoreEvent.gameWin: // Won the round
+            case ScoreEvent.gameLoss: // Lost the round
+                chain = 0;         // resets the score chain
+                score += scoreRun; // add scoreRun to total score
+                scoreRun = 0;      // reset scoreRun
+                break;
+            case ScoreEvent.mine: // Remove a mine card
+                chain++;           // increase the score chain
+                scoreRun += chain; // add score for this card to run
+                break;
+        }
+        // This second switch statement handles round wins and losses
+        switch (sEvt)
+        {
+            case ScoreEvent.gameWin:
+                // If it's a win, add the score to the next round
+                // static fields are NOT reset by Application.LoadLevel()
+                Prospector.SCORE_FROM_PREV_ROUND = score;
+                print("You won this round! Round score: " + score);
+                break;
+            case ScoreEvent.gameLoss:
+                // If it's a loss, check against the high score
+                if (Prospector.HIGH_SCORE <= score)
+                {
+                    print("You got the high score! High score: " + score);
+                    Prospector.HIGH_SCORE = score;
+                    PlayerPrefs.SetInt("ProspectorHighScore", score);
+                }
+                else
+                {
+                    print("Your final score for the game was: " + score);
+                }
+                break;
+            default:
+                print("score: " + score + "  scoreRun:" + scoreRun + "  chain:" + chain);
+                break;
+        }
     }
 
 }
